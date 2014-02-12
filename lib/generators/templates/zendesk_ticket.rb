34c450e1988b4f94d56f54a5813adcb8c86f0026
Andre Kramer <andre@wirelab.nl>
2014-02-12 14:33:39 +0100

    no need to output the env with <%= ...

--- lib/generators/templates/zendesk_ticket.rb
+++ lib/generators/templates/zendesk_ticket.rb
@@ -1,4 +1,4 @@
 ZendeskTicket.setup do |config|
-  config.api_token = "<%%= ENV['ZENDESK_API_TOKEN'] %>"
-  config.url = "<%%= ENV['ZENDESK_URL'] %>"
+  config.api_token = ENV['ZENDESK_API_TOKEN']
+  config.url = ENV['ZENDESK_URL']
 end
