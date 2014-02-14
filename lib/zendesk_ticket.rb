aa9bd1c054d4a533e43043f0c691dc9d477155df
Bart van Zon <bartvanzon@gmail.com>
2014-02-14 14:16:29 +0100

    minor syntax and whitespace cleanup

--- lib/zendesk_ticket.rb
+++ lib/zendesk_ticket.rb
@@ -3,19 +3,17 @@ require 'zendesk_api'
 module ZendeskTicket
   module Rails
     class Engine < ::Rails::Engine
-      initializer :assets, :group => :all do
-        ::Rails.application.config.assets.precompile += %w( zendesk_ticket.css zendesk_ticket.js)
+      initializer :assets, group: :all do
+        ::Rails.application.config.assets.precompile += %w( zendesk_ticket.css zendesk_ticket.js )
       end
     end
   end
-end
 
-module ZendeskTicket
   mattr_accessor :api_token
-  @@api_token = nil  
+  @@api_token = nil
 
   mattr_accessor :url
-  @@url = nil  
+  @@url = nil
 
   mattr_accessor :route
   @@route = 'ticket'
